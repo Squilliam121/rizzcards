43e7ca9f63b0a3f5818cda27bc9622970b773dcb
Squilliam121 <120696137+Squilliam121@users.noreply.github.com>
2025-03-08 11:36:34 +1300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+const text = document.getElementById("text");
+text.value = "";
+function alertTheUser(){
+    text.value = "U CLICKED ME :D";
+}
+
+function watchOut(){
+    text.value = "yippee";
+}
